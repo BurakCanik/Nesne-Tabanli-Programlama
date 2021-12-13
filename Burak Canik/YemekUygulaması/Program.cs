9dd4cec86a7e64464b29571a23c9315d6918cbd2
BurakCanik <94827305+BurakCanik@users.noreply.github.com>
2021-12-13 20:40:32 +0300

    Add files via upload

---
+++ Burak Canik/YemekUygulaması/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Yemek_Uygulaması
+{
+    class yemek
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string FoodName;
+            string material;
+
+            //  Name
+            Console.WriteLine("Enter Your Name");
+            name = Console.ReadLine();
+
+            // yemek
+            Console.WriteLine("enter the name of the dish : ");
+            FoodName = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("lütfen malzemeleri yazarken araya virgül koyunuz");
+            Console.WriteLine();
+
+            // ingredients for food
+            Console.WriteLine("Used materials");
+            material = Console.ReadLine();
+
+            Console.WriteLine("İsminiz: " + name);
+            Console.WriteLine("Yapılacak Yemeğin Adı: " + FoodName);
+            Console.WriteLine("Alınacak malzemeler: " + material);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
